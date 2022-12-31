95bb8489f28e0159ddb148e598cd024746eea6fd
tsubasamusu <rdr2daisuki@gmail.com>
2022-12-31 16:22:54 +0900

    他のプレイヤーを常に監視する処理を断念。

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,12 +1,6 @@
-using Cysharp.Threading.Tasks;
-using ExitGames.Client.Photon;
 using Photon.Pun;
-using Photon.Realtime;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TNRD;
-using UniRx.Triggers;
 using UnityEngine;
 
 namespace SchoolMetaverse
@@ -14,13 +8,10 @@ namespace SchoolMetaverse
     public class GameManager : MonoBehaviourPunCallbacks
     {
         [SerializeField]
-        private SpawnTranDetail spawnTranDetail;//SpawnTranDetail
-
-        [SerializeField]
         private PictureManager pictureManager;//PictureManager
 
         [SerializeField]
-        private Transform playersTran;//プレイヤーの親の位置情報
+        private Transform spawnTran;//スポーン地点
 
         [SerializeField]
         private List<SerializableInterface<ISetUp>> setUpList = new();//初期設定用インターフェイスのリスト
@@ -28,27 +19,14 @@ namespace SchoolMetaverse
         /// <summary>
         /// シーン遷移直後に呼び出される
         /// </summary>
-        /// <returns>待ち時間</returns>
-        private IEnumerator Start()
+        private void Start()
         {
             //プレイヤーのゲームオブジェクトを生成する
             GameObject objPlayer =
-                PhotonNetwork.Instantiate("Player", spawnTranDetail.transform.position, Quaternion.identity);
+                PhotonNetwork.Instantiate("Player", spawnTran.position, Quaternion.identity);
 
             //生成したオブジェクトの初期設定を行う
             objPlayer.GetComponent<PlayerController>().SetUp();
-
-            //生成したオブジェクトの親を設定する
-            objPlayer.transform.SetParent(playersTran);
-
-            //生成したオブジェクトを非活性化する
-            objPlayer.SetActive(false);
-
-            //スポーン可能になるまで待つ
-            yield return new WaitUntil(() => spawnTranDetail.CheckCanSpawn());
-
-            //生成したオブジェクトを活性化する
-            objPlayer.SetActive(true);
 
             //カメラの親を設定する
             Camera.main.transform.SetParent(objPlayer.transform);
