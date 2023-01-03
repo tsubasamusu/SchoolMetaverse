8216443fda2c5042a2ecae457d690b763d57143c
tsubasamusu <rdr2daisuki@gmail.com>
2023-01-03 15:02:26 +0900

    WebGLSupportをインポート。

--- Assets/Scripts/Manager/PictureManager.cs
+++ Assets/Scripts/Manager/PictureManager.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 
 namespace SchoolMetaverse
@@ -9,6 +10,8 @@ namespace SchoolMetaverse
     {
         private bool isChoosingPicture;//画像選択中かどうか
 
+        private Process exProcess;//起動する外部プロセス
+
         /// <summary>
         /// 画像選択中かどうか（取得用）
         /// </summary>
@@ -19,7 +22,41 @@ namespace SchoolMetaverse
         /// </summary>
         public void GetAndDisplayPicture()
         {
+            //画像選択中に変更する
+            isChoosingPicture= true;
 
+            //パスを取得する
+            string path = Application.dataPath + "/Plugins/GetPicture.exe";
+
+            //外部プロセスをインスタンス化する
+            exProcess = new Process();
+
+            //パスを登録する
+            exProcess.StartInfo.FileName = exProcess.StartInfo.Arguments = path;
+
+            //外部プロセスの終了を検知してイベントを発生させる
+            exProcess.EnableRaisingEvents = true;
+            exProcess.Exited += OnProcessExited;
+
+            //外部プロセスを実行する
+            exProcess.Start();
+        }
+
+        /// <summary>
+        /// 外部プロセスが終了した際に呼び出される
+        /// </summary>
+        /// <param name="sender">送信者</param>
+        /// <param name="eventArgs">EventArgs</param>
+        private void OnProcessExited(object sender, System.EventArgs eventArgs)
+        {
+            //ウィンドウを閉じる
+            exProcess.CloseMainWindow();
+
+            //外部プロセスを処分する
+            exProcess.Dispose();
+
+            //画像の選択をしていない状態に切り替える
+            isChoosingPicture= false;
         }
     }
 }
